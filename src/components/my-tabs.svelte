20bb1a26bd16d79b684a02ece27cf0beaa2c56cd
David Cabiness <dcabines@gmail.com>
2024-05-12 11:41:15 -0400

    nav

--- src/components/my-tabs.svelte
+++ src/components/my-tabs.svelte
@@ -3,7 +3,9 @@
   import Settings from "$lib/components/my-settings.svelte";
   import Page from "$lib/components/my-page.svelte";
   import Notifications from "$lib/components/my-card.svelte";
+  import JokeTax from "$lib/components/joke-tax.svelte";
   import * as Tabs from "$lib/components/ui/tabs/index.js";
+  import ModeSwitch from '$lib/components/mode-switch.svelte';
 </script>
 
 <Tabs.Root class="h-full" value="week">
@@ -13,6 +15,8 @@
       <Tabs.Trigger value="month">Settings</Tabs.Trigger>
       <Tabs.Trigger value="year">Page</Tabs.Trigger>
       <Tabs.Trigger value="Notifications">Notifications</Tabs.Trigger>
+      <Tabs.Trigger value="JokeTax">JokeTax</Tabs.Trigger>
+      <Tabs.Trigger value="ModeSwitch">ModeSwitch</Tabs.Trigger>
     </Tabs.List>
   </div>
   <Tabs.Content value="week" class="h-full">
@@ -26,5 +30,11 @@
   </Tabs.Content>
   <Tabs.Content value="Notifications" class="h-full">
     <Notifications />
+  </Tabs.Content>
+  <Tabs.Content value="JokeTax" class="h-full">
+    <JokeTax />
+  </Tabs.Content>
+  <Tabs.Content value="ModeSwitch" class="h-full">
+    <ModeSwitch />
   </Tabs.Content>
 </Tabs.Root>
